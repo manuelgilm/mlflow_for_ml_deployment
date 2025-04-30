2e87fb04db54af0144addec89c7e76a37ef46abe
Manuel Gil <manuelgilsitio@gmail.com>
2025-04-29 20:24:48 -0500

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+# MLflow for Machine Learning Deployment. 
+
+This repository explores the different approaches we can use to make a model available for use. 
+
+## Batch Inference
+
+## Deploy MLflow Model as a Local Inference Server
+
+### Iris Dataset usecase
+
+### Iris Image Dataset Usecase
+
+### 
---
+++ utils/decorators.py
@@ -0,0 +1,67 @@
+from utils.folder import get_project_root
+from utils.mlflow_exp import get_or_create_experiment
+import mlflow
+
+
+def mlflow_experiment(name: str, tags: dict):
+    """
+    Decorator to set the MLflow experiment name and tags.
+    This decorator creates a new experiment if it doesn't exist.
+    The decorator modifies the function to accept an `experiment` argument,
+    which is the MLflow experiment object.
+
+    :param name: The name of the MLflow experiment.
+    :param tags: A dictionary of tags to associate with the experiment.
+    :return: A decorator function that sets the MLflow experiment.
+    """
+
+    def decorator(func, name=name, tags=tags):
+        """
+        Decorator function to set the MLflow experiment name and tags.
+        It creates a new experiment if it doesn't exist.
+        The decorator modifies the function to accept an `experiment` argument,
+        which is the MLflow experiment object.
+
+        :param func: The function to be decorated.
+        :param name: The name of the MLflow experiment.
+        :param tags: A dictionary of tags to associate with the experiment.
+        :return: The decorated function with the MLflow experiment set.
+        """
+
+        def wrapper(*args, **kwargs):
+            print(f"Setting MLflow experiment: {name}")
+            experiment = get_or_create_experiment(name=name, tags=tags)
+            kwargs["mlflow_experiment"] = experiment
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def mlflow_client(func):
+    """
+    Decorator to pass the MLflow client as argument to the function.
+    """
+
+    def wrapper(*args, **kwargs):
+        print("Setting MLflow client...")
+        mlflow_client = mlflow.MlflowClient()
+        kwargs["mlflow_client"] = mlflow_client
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def mlflow_tracking_uri(func):
+    """
+    Set the MLflow tracking URI to the local file system.
+    """
+    print(func.__name__)
+
+    def wrapper(*args, **kwargs):
+        print("Setting MLflow tracking URI...")
+        mlflow.set_tracking_uri(uri=(get_project_root() / "mlruns").as_uri())
+        return func(*args, **kwargs)
+
+    return wrapper
